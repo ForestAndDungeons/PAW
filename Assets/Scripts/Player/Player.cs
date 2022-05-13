3a211854eb5061643a8505adfd345a0819ee6cb1
Gonzalo <gonzalo.puertas@davinci.edu.ar>
2022-05-13 05:45:10 -0300

    Player Death Animation
    Implemented Player Death Animation
    Improvements to Scene

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
     [SerializeField] string _horizontalAxis;
     [SerializeField] KeyCode _keyJump;
     [SerializeField] KeyCode _keyAttack;
+    [SerializeField] bool _isDead;
 
     //GroundSensor Variables.
     [Header("GroundSensor Variables")]
@@ -67,6 +68,7 @@ public class Player : MonoBehaviour
     {
         _renderer = this.GetComponent<MeshRenderer>();
         _collider = this.GetComponent<Collider>();
+        _isDead = false;
 
         _playerSoundManager = new PlayerSoundManager(_audioSource,_audioClip);
 
@@ -74,14 +76,14 @@ public class Player : MonoBehaviour
 
         _animationController = new AnimationController(_myAnimator);
 
-        _control = new Control(_movement, transform, _verticalAxis, _horizontalAxis, _animationController, _turnSpeed, _keyJump, _keyAttack, _playerSoundManager);
+        _control = new Control(_movement, transform, _verticalAxis, _horizontalAxis, _animationController, _turnSpeed, _keyJump, _keyAttack, _playerSoundManager, _isDead);
             
         _groundSensor = new GroundSensor(_radius, _groundLayer, transform);
 
         _playerBase = new PlayerBase(_name, _maxHealth, _attackPower, _armor, _playerSoundManager , _audioClip , _audioSource, _particleSystem, this, _animationController);
 
         _uiPlayer = new UIPlayer(_hearts, _spriteHeart);
-        
+
         _currentHealth = _playerBase.currentHealthGetter();
         _uiPlayer.UIArtificialUpdate(_maxHealth, _currentHealth);
     }
@@ -99,7 +101,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _control.IsometricMovement(_isGrounded);
+        _control.IsometricMovement(_isGrounded, _isDead);
     }
 
     public void OnTriggerEnter(Collider other)
@@ -127,7 +129,11 @@ public class Player : MonoBehaviour
 
     public void DisableThisObject()
     {
+        _isDead = true;
+        _control.isDeadSetter(_isDead);
+        Destroy(this.gameObject, 4f);
+
         //Add coroutine.
-        this.gameObject.SetActive(false);
+        //this.gameObject.(false);
     }
 }
