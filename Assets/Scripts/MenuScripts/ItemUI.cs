e5dd2268dffe8c8fe1d239414a423cbe67e64c84
FerrerGustavo <ferrergustavo97@gmail.com>
2022-07-01 21:12:49 -0300

    Add sound Colectable_UI

--- Assets/Scripts/MenuScripts/ItemUI.cs
+++ Assets/Scripts/MenuScripts/ItemUI.cs
@@ -6,15 +6,25 @@ using TMPro;
 
 public class ItemUI : MonoBehaviour
 {
+    [Header("TMP Variables")]
     [SerializeField] TMP_Text _title;
     [SerializeField] TMP_Text _description;
+    [Header("Animator")]
     [SerializeField] Animator _animController;
+    [Header("AudioSource")]
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _audioClip;
 
     public void ActiveInterface(string title, string description)
     {
         _title.text = title;
         _description.text = description;
         _animController.SetTrigger("IsActive");
+    }
+
+    public void popUp_Sound()
+    {
+        _audioSource.PlayOneShot(_audioClip);
     }
 
 }
